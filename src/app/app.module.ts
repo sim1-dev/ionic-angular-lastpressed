47f7507185d5452c5eed7827539d415178f4483e
sim1-dev <simonetenisci10@gmail.com>
2022-08-19 21:43:35 +0200

    Globalization provider fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { Globalization } from '@awesome-cordova-plugins/globalization/ngx';
 import { ThemeDetection } from '@awesome-cordova-plugins/theme-detection/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -13,7 +14,7 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ThemeDetection],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ThemeDetection, Globalization],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
